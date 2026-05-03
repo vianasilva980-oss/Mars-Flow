ca72bc1f2e697b32fb43e1f43e29dd210df248e5
Recife <portaria.recife@effem.com>
2026-05-03 16:21:19 -0300

    atualização do sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // No topo do seu sw.js
-const VERSION = '1.0.4';
+const VERSION = '1.0.5';
 
 self.addEventListener('message', (event) => {
     if (event.data === 'GET_VERSION') {
@@ -10,7 +10,7 @@ self.addEventListener('message', (event) => {
     }
 });
 
-const CACHE_NAME = 'mars-flow-v01';
+const CACHE_NAME = 'mars-flow-v02';
 
 // Lista de ativos (Mantenha exatamente como você definiu)
 const ASSETS = [
